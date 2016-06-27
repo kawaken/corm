cecbf9afbf765410d15a6ccf951595457d42dc1e
Kentaro Kawano <kentaro.kawano@gmail.com>
2016-06-28 00:38:47 +0900

    _corm配下のpakcageをvendorにエクスポートできるようにした

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 )
 
-const cormfilename = "Cormfile"
+const (
+	cormFilename = "Cormfile"
+	cormDir      = "_corm"
+)
 
 var errCannotParseLine = errors.New("cannot parse line")
+
+var vcsMetaDir = []string{".svn", ".git", ".hg"}
 
 type repository struct {
 	Path   string
@@ -70,14 +76,52 @@ func goGet(repo *repository) error {
 	return err
 }
 
+func newCopyFileFun(srcBase string, destBase string) filepath.WalkFunc {
+
+	return func(srcPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(srcBase, srcPath)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(destBase, relPath)
+
+		if info.IsDir() {
+			index := sort.SearchStrings(vcsMetaDir, info.Name())
+			if index < len(vcsMetaDir) {
+				return filepath.SkipDir
+			}
+
+			return os.MkdirAll(destPath, info.Mode())
+		}
+
+		if exists(destPath) {
+			return nil
+		}
+
+		return os.Link(srcPath, destPath)
+	}
+
+}
+
+func export(src string, dest string) error {
+	copyFile := newCopyFileFun(src, dest)
+	filepath.Walk(src, copyFile)
+	return nil
+}
+
 func mainCmd() int {
-	dir, err := os.Getwd()
+	curdir, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "cannot get current directory")
 		return 1
 	}
 
-	cormfile := filepath.Join(dir, cormfilename)
+	cormfile := filepath.Join(curdir, cormFilename)
 	if !exists(cormfile) {
 		fmt.Fprintf(os.Stderr, "%s does not exists\n", cormfile)
 		return 1
@@ -94,9 +138,9 @@ func mainCmd() int {
 		return 1
 	}
 
-	vendorDir := filepath.Join(dir, "_vendor")
+	dirtyVendorDir := filepath.Join(curdir, cormDir)
 	gopath := os.Getenv("GOPATH")
-	os.Setenv("GOPATH", fmt.Sprintf("%s:%s", vendorDir, gopath))
+	os.Setenv("GOPATH", fmt.Sprintf("%s:%s", dirtyVendorDir, gopath))
 
 	for _, repo := range repos {
 		err := goGet(repo)
@@ -105,9 +149,16 @@ func mainCmd() int {
 		}
 	}
 
+	/*
+		dirtyVendorSrcDir := filepath.Join(dirtyVendorDir, "src")
+		cleanVendorDir := filepath.Join(curdir, "vendor")
+		export(dirtyVendorSrcDir, cleanVendorDir)
+	*/
+
 	return 0
 }
 
 func main() {
+	sort.Strings(vcsMetaDir)
 	os.Exit(mainCmd())
 }
